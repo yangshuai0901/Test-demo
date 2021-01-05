3d497e815a6ce3a526089c5a2f92f16ce4a51729
yangshuai <2237632828@qq.com>
2021-01-05 10:48:22 +0800

    提交内容的描述

---
+++ src/main/java/Demo.java
@@ -0,0 +1,10 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demo {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Book book = (Book)context.getBean("book2");
+        System.out.println(book.getName());
+    }
+}
