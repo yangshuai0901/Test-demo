dc43f0546572948ccf54983915ace0540225e568
yangshuai <2237632828@qq.com>
2021-01-05 10:53:43 +0800

    测试

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -6,5 +6,6 @@ public class Demo {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Book book = (Book)context.getBean("book2");
         System.out.println(book.getName());
+        //提交
     }
 }
